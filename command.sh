d1e1d6764d0b4b63359b5c196c6aac2925e48906
Ahmad Zahradeen Tahir <aztahir20@gmail.com>
2023-02-06 02:33:52 +0100

    Added all commands

---
+++ command.sh
@@ -0,0 +1,7 @@
+npx create react app blackbird
+git init   
+git commit -m "initialized react app"
+git checkout -b updated_logo
+git commit -m "replaced logo and link"
+git push -u origin master
+# REPO_URL <https://github.com/Ahmad-Zahradeen-Tahir/blackbird>
